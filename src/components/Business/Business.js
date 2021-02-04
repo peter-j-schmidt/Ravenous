ce0878af429eca2253c9e2cefa7b63741ed919fd
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-02-04 11:44:00 -0500

    Injected missing JavaScript.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Business.css';
 
-let business = {
+const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
     name: 'MarginOtto Pizzeria',
     address: '1010 Paddington Way',
@@ -18,18 +18,18 @@ class Business extends React.Component {
        return (
         <div class="Business">
             <div className="image-container">
-            <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+            <img src={business.imageSrc} alt=''/>
             </div>
             <h2>{business.name}</h2>
             <div className="Business-information">
                 <div className="Business-address">
                     <p>{business.address}</p>
                     <p>{business.city}</p>
-                    <p>{business.state} {business.zipCode}</p>
+                    <p>{`${business.state} ${business.zipCode}`}</p>
                 </div>
                 <div className="Business-reviews">
-                    <h3>{business.category}</h3>
-                    <h3 className="rating">{business.rating} stars</h3>
+                    <h3>{business.category.toUpperCase()}</h3>
+                    <h3 className="rating">{` ${business.rating} stars`}</h3>
                     <p>{business.reviewCount} reviews</p>
                 </div>
             </div>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-let sortByOptions = {
+const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
@@ -10,7 +10,8 @@ let sortByOptions = {
 class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOptions => {
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
         });
     }
 
